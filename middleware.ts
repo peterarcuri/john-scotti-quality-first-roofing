c1d6b460bf5c878699319dddce69f43044ef8a08
Petersix <150849737+peterarcuri@users.noreply.github.com>
2025-06-09 22:52:00 -0400

    update middleware for Clerk domain issue

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware({
-  proxyUrl: 'https://john-scotti-quality-first-roofing.vercel.app',
+  
 });
 
 export const config = {
